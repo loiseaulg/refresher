8783c55f1195b6166188e8d4af7d2b05df4f1bcc
Gabriel Loiseau <loiseaulg@gmail.com>
2020-09-25 08:00:20 +0200

    Add remove methods to Graph class

--- graph.py
+++ graph.py
@@ -18,3 +18,12 @@ class Graph:
     
     def addVertice(self, v1, v2): # addVertice(self, v1, v2) != addVertice(self, v2, v1)
         self.graph[v1] = v2
+    
+    def removeVertice(self, v):
+        self.graph[v] = self.graph[v].remove(v)
+    
+    def removeNode(self,p):
+        del self.graph[p]
+        for keys in self.graph.keys:
+            self.graph[keys].remove(p)
+
